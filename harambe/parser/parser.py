92e94a486350d7a33f29d5b0707662011b6ffb84
asim-shrestha <asim.shrestha@hotmail.com>
2024-03-29 12:11:03 -0700

    ✨ Parser stuff

--- harambe/parser/parser.py
+++ harambe/parser/parser.py
@@ -8,6 +8,7 @@ class SchemaParser(ABC):
     """
     Interface for schema parsers
     """
+
     @abstractmethod
     def validate(self, data: Dict[str, Any]) -> None:
         pass
@@ -26,15 +27,17 @@ class PydanticSchemaParser(SchemaParser):
         self.model(**data)
 
 
-def _schema_to_pydantic_model(schema: Dict[str, Any], model_name: str = 'DynamicModel') -> Type[BaseModel]:
+def _schema_to_pydantic_model(
+    schema: Dict[str, Any], model_name: str = "DynamicModel"
+) -> Type[BaseModel]:
     """
     Convert a JSON schema to a Pydantic model dynamically. All fields are optional
     """
     fields = {}
 
     for field_name, field_info in schema.items():
-        field_description = field_info.get('description', None)
-        field_type = field_info.get('type')
+        field_description = field_info.get("description", None)
+        field_type = field_info.get("type")
 
         if field_type == OBJECT_TYPE:
             python_type = _schema_to_pydantic_model(
@@ -48,7 +51,7 @@ def _schema_to_pydantic_model(schema: Dict[str, Any], model_name: str = 'Dynamic
         fields[field_name] = (python_type, Field(..., description=field_description))
 
     # Disallow additional fields
-    config = {'extra': Extra.forbid}
+    config = {"extra": Extra.forbid}
 
     return create_model(model_name, __config__=config, **fields)
 
@@ -60,22 +63,19 @@ def get_type(field: str) -> Type:
     return field_type
 
 
-OBJECT_TYPE = 'object'
-LIST_TYPE = 'array'
+OBJECT_TYPE = "object"
+LIST_TYPE = "array"
 COMPLEX_TYPES = [OBJECT_TYPE, LIST_TYPE]
 BASIC_FIELD_TYPE_MAPPING = {
     "string": str,
     "str": str,
-
     "boolean": bool,
     "bool": bool,
-
     "integer": int,
     "int": int,
     "number": float,
     "float": float,
     "double": float,
-
     # TODO: Add support for date and datetime types
     # TODO: The URL type should have a custom validator to handle relative URLs
     "url": AnyUrl,
--- tests/parser/schemas.py
+++ tests/parser/schemas.py
@@ -1,35 +1,54 @@
 document_schema = {
     "title": {"type": "string", "description": "The name of the document"},
-    "document_url": {"type": "string", "actions": {"download": True}, "description": "A link to the document"}
+    "document_url": {
+        "type": "string",
+        "actions": {"download": True},
+        "description": "A link to the document",
+    },
 }
 
 documents_schema = {
     "documents": {
-        "type": "array", "items": {
-            "type": "object", "properties": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
                 "title": {"type": "string", "description": "The name of the document"},
-                "document_url": {"type": "string", "description": "A link to the document"}
-            }
-        }
+                "document_url": {
+                    "type": "string",
+                    "description": "A link to the document",
+                },
+            },
+        },
     }
 }
 
 contact_schema = {
-    "name": {"type": "object", "properties": {
-        "first_name": {"type": "string", "description": "The first name of the contact"},
-        "last_name": {"type": "string", "description": "The last name of the contact"},
-    }},
-    "address": {"type": "object", "properties": {
-        "street": {"type": "string", "description": "The street of the address"},
-        "city": {"type": "string", "description": "The city of the address"},
-        "zip": {"type": "int", "description": "The zip code of the address"},
-    }}
+    "name": {
+        "type": "object",
+        "properties": {
+            "first_name": {
+                "type": "string",
+                "description": "The first name of the contact",
+            },
+            "last_name": {
+                "type": "string",
+                "description": "The last name of the contact",
+            },
+        },
+    },
+    "address": {
+        "type": "object",
+        "properties": {
+            "street": {"type": "string", "description": "The street of the address"},
+            "city": {"type": "string", "description": "The city of the address"},
+            "zip": {"type": "int", "description": "The zip code of the address"},
+        },
+    },
 }
 
 # Schema with a list of strings
-list_of_strings_schema = {
-    "tags": {"type": "array", "items": {"type": "string"}}
-}
+list_of_strings_schema = {"tags": {"type": "array", "items": {"type": "string"}}}
 
 # Schema with a list of objects
 list_of_objects_schema = {
@@ -37,11 +56,8 @@ list_of_objects_schema = {
         "type": "array",
         "items": {
             "type": "object",
-            "properties": {
-                "name": {"type": "string"},
-                "email": {"type": "string"}
-            }
-        }
+            "properties": {"name": {"type": "string"}, "email": {"type": "string"}},
+        },
     }
 }
 
@@ -51,23 +67,14 @@ object_with_list_schema = {
         "type": "object",
         "properties": {
             "name": {"type": "string"},
-            "members": {
-                "type": "array",
-                "items": {"type": "string"}
-            }
-        }
+            "members": {"type": "array", "items": {"type": "string"}},
+        },
     }
 }
 
 # Schema for a list of lists
 list_of_lists_schema = {
-    "matrix": {
-        "type": "array",
-        "items": {
-            "type": "array",
-            "items": {"type": "int"}
-        }
-    }
+    "matrix": {"type": "array", "items": {"type": "array", "items": {"type": "int"}}}
 }
 
 # Schema for lists with nested object items that have their own lists
@@ -84,14 +91,11 @@ nested_lists_and_objects_schema = {
                         "type": "object",
                         "properties": {
                             "team_name": {"type": "string"},
-                            "members": {
-                                "type": "array",
-                                "items": {"type": "string"}
-                            }
-                        }
-                    }
-                }
-            }
-        }
+                            "members": {"type": "array", "items": {"type": "string"}},
+                        },
+                    },
+                },
+            },
+        },
     }
 }
