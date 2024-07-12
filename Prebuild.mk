773a3c39291190f214ebfe7bd3dd46558f2452af
awtkns <32209255+awtkns@users.noreply.github.com>
2024-07-11 18:26:54 -0700

    🫡 Enqueue coro + e2e testing

--- Prebuild.mk
+++ Prebuild.mk
@@ -5,5 +5,5 @@ RESOLVE_DEPS: ## Install required dependencies
 	@pip install poetry==1.6.1
 	@poetry config virtualenvs.create false
 	@poetry install --no-root
-	@playwright install-deps chromium && playwright install chromium
+	@poetry run playwright install-deps chromium && poetry run playwright install chromium
 .PHONY: RESOLVE_DEPS
