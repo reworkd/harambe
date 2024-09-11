ca7be47a8c0b8107b316b8ee9c176f9a19ab1908
seanmcguire12 <seanmcguire1@outlook.com>
2024-09-11 13:36:50 -0700

    update init

--- harambe/__init__.py
+++ harambe/__init__.py
@@ -2,6 +2,12 @@ from .core import PAGE_PDF_FILENAME, SDK, AsyncScraper
 from .parser.schemas import Schemas
 from .types import AsyncScraperType, ScrapeResult
 from .utils import PlaywrightUtils
+from .errors import (
+    HarambeException,
+    SchemaValidationError,
+    MissingRequiredFieldError,
+    CaptchaError,
+)
 
 __all__ = [
     "ScrapeResult",
@@ -11,4 +17,8 @@ __all__ = [
     "AsyncScraperType",
     "AsyncScraper",
     "PAGE_PDF_FILENAME",
+    "HarambeException",
+    "SchemaValidationError",
+    "MissingRequiredFieldError",
+    "CaptchaError",
 ]
