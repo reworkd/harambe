a3ac1089f0a8f66f96c6e7f2af2d3e5b0007c1ca
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2024-09-12 02:13:51 +0300

    feat: Add save_cookies function to SDK

--- harambe/__init__.py
+++ harambe/__init__.py
@@ -2,6 +2,17 @@ from .core import PAGE_PDF_FILENAME, SDK, AsyncScraper
 from .parser.schemas import Schemas
 from .types import AsyncScraperType, ScrapeResult
 from .utils import PlaywrightUtils
+from harambe.observer import (
+    LocalStorageObserver,
+    LoggingObserver,
+)
+
+
+def save_cookies(page, domain, stage):
+    # Initialize SDK with necessary observers
+    observers = [LocalStorageObserver(domain, stage), LoggingObserver()]
+    sdk = SDK(page, domain=domain, stage=stage, observer=observers)
+    return sdk.save_cookies()
 
 
 __all__ = [
@@ -12,4 +23,5 @@ __all__ = [
     "AsyncScraperType",
     "AsyncScraper",
     "PAGE_PDF_FILENAME",
+    "save_cookies"
 ]
