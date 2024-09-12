e5a68ede8ece9e81d819eaf32f7b32b5ec7445bf
awtkns <32209255+awtkns@users.noreply.github.com>
2024-09-11 23:23:56 -0700

    🙋‍♂️ Allow Config of Schema Parsing
    Supports all standard pydantic schema options.
    Allows us to ignore / include extra fields

--- harambe/parser/parser.py
+++ harambe/parser/parser.py
@@ -1,18 +1,15 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Optional, Type, Union, Dict
 
-from pydantic import BaseModel, Extra, Field, NameEmail, ValidationError, create_model
+from pydantic import BaseModel, Field, NameEmail, ValidationError, create_model, ConfigDict
 
+from harambe.parser.type_currency import ParserTypeCurrency
 from harambe.parser.type_date import ParserTypeDate
 from harambe.parser.type_enum import ParserTypeEnum
 from harambe.parser.type_number import ParserTypeNumber
 from harambe.parser.type_phone_number import ParserTypePhoneNumber
-from harambe.parser.type_currency import ParserTypeCurrency
 from harambe.parser.type_url import ParserTypeUrl
-from harambe.types import URL, Schema, ScrapeResult
-
-OBJECT_TYPE = "object"
-LIST_TYPE = "array"
+from harambe.types import URL, Schema, ScrapeResult, SchemaFieldType
 
 
 class SchemaParser(ABC):
@@ -43,9 +40,9 @@ class PydanticSchemaParser(SchemaParser):
 
     def __init__(self, schema: Schema):
         self.schema = schema
-        self.field_types: dict[str, Any] = {}
+        self.field_types: dict[SchemaFieldType, Any] = {}
 
-    def validate(self, data: dict[str, Any], base_url: URL) -> dict[str, Any]:
+    def validate(self, data: dict[SchemaFieldType, Any], base_url: URL) -> dict[str, Any]:
         # Set these values here for convenience to avoid passing them around. A bit hacky
         self.field_types = self._get_field_types(base_url)
         self.model = self._schema_to_pydantic_model(self.schema)
@@ -64,7 +61,7 @@ class PydanticSchemaParser(SchemaParser):
             )
 
     @staticmethod
-    def _get_field_types(base_url: str) -> dict[str, Any]:
+    def _get_field_types(base_url: str) -> dict[SchemaFieldType, Any]:
         return {
             "string": str,
             "str": str,
@@ -78,8 +75,8 @@ class PydanticSchemaParser(SchemaParser):
             "currency": ParserTypeCurrency(),
             "email": NameEmail,
             "enum": ParserTypeEnum,
-            LIST_TYPE: List,
-            OBJECT_TYPE: dict[str, Any],
+            "array": list,
+            "object": dict[str, Any],
             "datetime": ParserTypeDate(),
             "phone_number": ParserTypePhoneNumber(),
             "url": ParserTypeUrl(base_url=base_url),
@@ -95,27 +92,26 @@ class PydanticSchemaParser(SchemaParser):
         if item_type is None:
             raise ValueError(f"Item type for array `{model_name}` is missing")
 
-        if item_type == OBJECT_TYPE:
-            python_type = self._schema_to_pydantic_model(
+        if item_type == "object":
+            return self._schema_to_pydantic_model(
                 items_info.get("properties", {}),
                 model_name=f"{model_name}Object",
             )
-        elif item_type == LIST_TYPE:
-            # Lists can't be null
-            python_type = List[  # type: ignore
+
+        if item_type == "array":
+            return List[  # type: ignore
                 self._items_schema_to_python_type(
                     items_info.get("items", {}),
                     model_name=f"{model_name}Item",
                 )
             ]
-        elif item_type == "enum":
-            # Every enum has its own unique variants
-            python_type = self._get_type(item_type)(items_info["variants"])
-        else:
-            # Non complex types aren't optional when they're within a list
-            python_type = self._get_type(item_type)
 
-        return python_type
+        # Every enum has its own unique variants
+        if item_type == "enum":
+            return self._get_type(item_type)(items_info["variants"])
+
+        return self._get_type(item_type)
+
 
     def _schema_to_pydantic_model(
         self, schema: Schema, model_name: str = "DynamicModel"
@@ -124,18 +120,22 @@ class PydanticSchemaParser(SchemaParser):
         Convert a JSON schema to a Pydantic model dynamically. All fields are optional
         """
         fields = {}
+        config: ConfigDict = {"extra": "forbid"}
+        config.update(schema.get("__config__", {}))
 
         for field_name, field_info in schema.items():
+            if field_name == "__config__":
+                continue
+
             field_description = field_info.get("description", None)
             field_type = field_info.get("type")
 
-            if field_type == OBJECT_TYPE:
+            if field_type == "object":
                 python_type = self._schema_to_pydantic_model(
                     field_info.get("properties", {}) or {},
                     model_name=f"{model_name}{field_name.capitalize()}",
                 )
-            elif field_type == LIST_TYPE:
-                # Lists can't be null
+            elif field_type == "list":
                 python_type = List[  # type: ignore
                     self._items_schema_to_python_type(
                         field_info.get("items", {}) or {},
@@ -143,19 +143,14 @@ class PydanticSchemaParser(SchemaParser):
                     )
                 ]
             elif field_type == "enum":
-                # Every enum has its own unique variants
                 python_type = self._get_type(field_type)(field_info["variants"])
             else:
-                # Non complex types should be optional
                 python_type = Optional[self._get_type(field_type)]  # type: ignore
 
             fields[field_name] = (
                 python_type,
                 Field(..., description=field_description),
             )
-
-        # Disallow additional fields
-        config = {"extra": Extra.forbid}
 
         return create_model(model_name, __config__=config, **fields)  # type: ignore
 
