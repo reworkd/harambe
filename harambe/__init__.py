7070b8d473fed429d07991c5d3babb287e08c8c7
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2024-09-12 21:29:57 +0300

    Remove unused observer imports

--- harambe/__init__.py
+++ harambe/__init__.py
@@ -2,11 +2,6 @@ from .core import PAGE_PDF_FILENAME, SDK, AsyncScraper
 from .parser.schemas import Schemas
 from .types import AsyncScraperType, ScrapeResult
 from .utils import PlaywrightUtils
-from harambe.observer import (
-    LocalStorageObserver,
-    LoggingObserver,
-)
-
 __all__ = [
     "ScrapeResult",
     "Schemas",
