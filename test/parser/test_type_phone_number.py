1e3e2ab12a9db2ed67b5bb2b2c600b5a92b6c360
asim-shrestha <asim.shrestha@hotmail.com>
2024-10-04 15:32:06 -0700

    📞 Phone number parsing again

--- test/parser/test_type_phone_number.py
+++ test/parser/test_type_phone_number.py
@@ -6,49 +6,28 @@ from harambe.parser.type_phone_number import ParserTypePhoneNumber
 @pytest.mark.parametrize(
     "phone_number",
     [
-        # 0
         "(+1) 415-155-1555",
-        # 1
         "11111111111",
-        # 2
         "911",
-        # 3
         "(+4) 1111111111 (Extension: 323)",
-        # 4
+        "(956) 318-2626, Ext: 4857", # Extension with parenthesis
         "206-555-7115, ext. 239",  # Domestic, with extension
-        # 5
         "212-456-7890",  # Domestic
-        # 6
         "456-7890",  # Local Phone Number
-        # 7
         "+1-212-456-7890",  # International Phone Number
-        # 8
         "1-212-456-7890",  # Dialed in the US
-        # 9
         "001-212-456-7890",  # Dialed in Germany
-        # 10
         "191-212-456-7890",  # Dialed in France
-        # 11
         "2124567890",
-        # 12
         "212-456-7890",
-        # 13
         "(212)456-7890",
-        # 14
         "(212)-456-7890",
-        # 15
         "212.456.7890",
-        # 16
         "212 456 7890",
-        # 17
         "+12124567890",
-        # 18
         "+12124567890",
-        # 19
         "+1 212.456.7890",
-        # 20
         "+212-456-7890",
-        # 21
         "1-212-456-7890",
     ],
 )
