bf3883857b39b6178ef446c97deadda5a73dace1
asim-shrestha <asim.shrestha@hotmail.com>
2024-10-22 15:20:28 -0700

    🐛 Fix

--- harambe/types.py
+++ harambe/types.py
@@ -69,23 +69,6 @@ class SetCookieParam(TypedDict, total=False):
     sameSite: Optional[Literal["Lax", "None", "Strict"]]
 
 
-class HarnessOptions(TypedDict, total=False):
-    browser_type: BrowserType
-    headless: bool
-    stealth: bool
-    default_timeout: int
-    user_agent: str
-    proxy: Optional[str]
-    cdp_endpoint: Optional[str]
-    cookies: Sequence[SetCookieParam]
-    headers: Optional[dict[str, str]]
-    viewport: Optional[ViewportSize]
-    abort_unnecessary_requests: bool
-    disable_go_to_url: bool
-    on_start: Optional[Callback]
-    on_end: Optional[Callback]
-
-
 class Cookie(TypedDict):
     name: str
     value: str
@@ -104,3 +87,21 @@ class LocalStorage(TypedDict):
     path: str | None
     key: str
     value: str
+
+
+class HarnessOptions(TypedDict, total=False):
+    browser_type: BrowserType
+    headless: bool
+    stealth: bool
+    default_timeout: int
+    user_agent: str
+    proxy: Optional[str]
+    cdp_endpoint: Optional[str]
+    cookies: Sequence[SetCookieParam]
+    local_storage: Sequence[LocalStorage]
+    headers: Optional[dict[str, str]]
+    viewport: Optional[ViewportSize]
+    abort_unnecessary_requests: bool
+    disable_go_to_url: bool
+    on_start: Optional[Callback]
+    on_end: Optional[Callback]
