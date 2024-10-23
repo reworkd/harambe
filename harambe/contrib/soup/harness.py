f2f2bf72da349b0c1a7e3ded817d1b8ff98a3251
Adam Watkins <cadamrun@gmail.com>
2024-10-23 15:44:29 -0700

    Mitm (#80)
    * 🌐🔗 Changed harnesses to allow for using mitm proxies for upstreaming
    
    * Reverted MITM to be toggled/passed through headers instead
    
    ---------
    
    Co-authored-by: Kunwar Sodhi <kunwarsodhi@Kunwars-MBP.localdomain>

--- harambe/contrib/soup/harness.py
+++ harambe/contrib/soup/harness.py
@@ -5,6 +5,7 @@ from curl_cffi.requests import AsyncSession
 
 from harambe.contrib.soup.impl import SoupPage
 from harambe.contrib.soup.tracing import Tracer
+from harambe.proxy import proxy_from_url
 from harambe.types import SetCookieParam
 
 Callback = Callable[[Tracer], Awaitable[None]]
@@ -21,6 +22,11 @@ async def soup_harness(
     on_end: Optional[Callback] = None,
     **__: Any,
 ) -> AsyncGenerator[PageFactory, None]:
+    if headers is not None:
+        header_keys = headers.keys()
+        if "X-MITM-PROXY" in header_keys and 'X-MITM-ADDRESS' in header_keys:
+            proxy = headers['X-MITM-ADDRESS']
+
     async with AsyncSession(proxy=proxy, impersonate="chrome", verify=False) as s:
         for c in cookies:
             s.cookies.set(
