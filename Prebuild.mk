b002326b0eabc898c0ae31ff822e353ad3438ca7
awtkns <32209255+awtkns@users.noreply.github.com>
2024-10-29 23:20:17 -0700

    🫡 Migrate Harambe to UV

--- Prebuild.mk
+++ Prebuild.mk
@@ -2,10 +2,7 @@
 
 
 RESOLVE_DEPS: ## Install required dependencies
-	@pip install poetry==1.6.1
-	@poetry config virtualenvs.create false
-	@poetry install --no-root
-	@poetry run playwright install-deps chromium && poetry run playwright install chromium
-	@poetry run playwright install-deps firefox && poetry run playwright install firefox
-	@poetry run playwright install-deps webkit && poetry run playwright install webkit
+	@uv run playwright install-deps chromium && uv run playwright install chromium
+	@uv run playwright install-deps firefox && uv run playwright install firefox
+	@uv run playwright install-deps webkit && uv run playwright install webkit
 .PHONY: RESOLVE_DEPS
