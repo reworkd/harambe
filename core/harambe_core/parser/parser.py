cadcee7fcdad163db164e717159d45f3e7bf758e
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2024-11-06 01:03:52 +0200

    Refactor currency parsing to use ParserTypePrice

--- core/harambe_core/parser/parser.py
+++ core/harambe_core/parser/parser.py
@@ -11,7 +11,7 @@ from pydantic import (
 
 from harambe_core.errors import SchemaValidationError
 from harambe_core.parser.expression import ExpressionEvaluator
-from harambe_core.parser.type_currency import ParserTypeCurrency
+from harambe_core.parser.type_price import ParserTypePrice
 from harambe_core.parser.type_date import ParserTypeDate
 from harambe_core.parser.type_email import ParserTypeEmail
 from harambe_core.parser.type_enum import ParserTypeEnum
@@ -76,7 +76,7 @@ class SchemaParser:
             "number": ParserTypeNumber,
             "float": ParserTypeNumber,
             "double": ParserTypeNumber,
-            "currency": ParserTypeCurrency(),
+            "currency": ParserTypePrice(),
             "email": ParserTypeEmail,
             "enum": ParserTypeEnum,
             "array": list,
