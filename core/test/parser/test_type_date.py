2154e0d2d0870f0c58202aaf6e9e8a467e51553c
Mohamed Khalil <69445107+mohamedmamdouh22@users.noreply.github.com>
2024-11-23 05:30:01 +0200

    🗓️ Graceful Handling of Non-Specific Dates  (#92)
    * Refactor ParserTypeDate to allow nullable objects and handle common non-specific dates
    * Bump version to 0.51.1 in core and sdk pyproject.toml files
    * Refactor ParserTypeDate to allow nullable objects and handle common non-specific dates
    * Refactor test_pydantic_type_date_validate_type_error in test_type_date.py
    * Refactor test_pydantic_type_date_validate_type_error in test_type_date.py
    * Refactor import in test_type_date.py

--- core/test/parser/test_type_date.py
+++ core/test/parser/test_type_date.py
@@ -1,42 +1,73 @@
 import pytest
-
+from datetime import datetime
 from harambe_core.parser.type_date import ParserTypeDate
 
 
+def assert_is_iso_format(date_string):
+    try:
+        datetime.fromisoformat(date_string)
+    except ValueError:
+        assert False, f"Date string '{date_string}' is not in ISO 8601 format."
+
+
 @pytest.mark.parametrize(
     "date_string",
     [
-        # 0
         "05/29/2024 02:00:00 PM",
-        # 1
         "5/22/2024 7:55:50 AM (EST)",
-        # 2
         "May 20 2024 2:00PM",
-        # 3
         "May 14, 2024 - 2:00pm",
-        # 4
         "4/30/2024 09:00:02 AM (CT)",
-        # 5
         "1995-12-17T03:24:00",
-        # 6
         "2020-05-12T23:50:21.817Z",
-        # 7
         "December 17, 1995 03:24:00",
-        # 8
         "2019-01-01T00:00:00.000+00:00",
+        "2024-11-23T18:30:00",
+        "11/23/24 06:30 PM",
+        "November 23rd, 2024 at 6:30pm",
+        "Nov 23 2024 6:30 PM (GMT+1)",
+        "23rd November 2024, 18:30",
     ],
 )
 def test_pydantic_type_date_validate_type_success(date_string):
-    assert ParserTypeDate.validate_type(date_string)
+    parsed_date = ParserTypeDate.validate_type(date_string)
+    assert isinstance(
+        parsed_date, str
+    ), f"Expected string for '{date_string}', got {parsed_date}"
+    assert_is_iso_format(parsed_date)
 
 
 @pytest.mark.parametrize(
     "date_string",
     [
-        # 0
-        "",  # ❌ Empty string
-        # 1
-        123,  # ❌ Number instead of string
+        "TBD",
+        "TBA",
+        "TBC",
+        "Open Until Filled",
+        "Open Until Contracted",
+        "To Be Confirmed",
+        "As Soon As Possible",
+        "Subject to Change",
+        "Not Specified",
+        "Ongoing",
+        "Continuous",
+        "Pending Approval",
+        "N/A",
+        "Unknown",
+    ],
+)
+def test_parser_handles_non_specific_dates_correctly(date_string):
+    parsed_date = ParserTypeDate.validate_type(date_string)
+    assert parsed_date is None, f"Expected None for '{date_string}', got {parsed_date}"
+
+
+@pytest.mark.parametrize(
+    "date_string",
+    [
+        "",
+        123,
+        "InvalidDateFormat",
+        "32/13/2024",
     ],
 )
 def test_pydantic_type_date_validate_type_error(date_string):
