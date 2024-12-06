79c89af5e466e813a9c73c9a88dee4a497c02b24
asim-shrestha <asim.shrestha@hotmail.com>
2024-12-06 10:26:56 -0800

    ✨ Converters

--- sdk/harambe/html_converter/__init__.py
+++ sdk/harambe/html_converter/__init__.py
@@ -9,11 +9,11 @@ HTMLConverterType = Literal["markdown", "text"]
 
 
 def get_html_converter(
-    html_converter_type: HTMLConverterType | None,
+    html_converter_type: HTMLConverterType,
 ) -> MarkdownConverter:
     if html_converter_type == "markdown":
         return HTMLToMarkdownConverter()
-    if html_converter_type == "text":
+    elif html_converter_type == "text":
         return HTMLToTextConverter()
-    else:
-        raise UnknownHTMLConverter(html_converter_type)
+
+    raise UnknownHTMLConverter(html_converter_type)
