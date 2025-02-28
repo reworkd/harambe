9fcf9f7c696759be6cf3f972c7e14ba0e528232a
kunwarsodhi <kunwar@reworkd.ai>
2025-02-27 20:50:32 -0800

    Added option to disable service workers

--- sdk/harambe/contrib/playwright/harness.py
+++ sdk/harambe/contrib/playwright/harness.py
@@ -40,6 +40,7 @@ async def playwright_harness(
     enable_clipboard: bool = False,
     launch_args: Sequence[str] = (),
     extensions: Sequence[str] = (),
+    disable_service_workers: bool = False,
     **__: Any,
 ) -> AsyncGenerator[PageFactory, None]:
     """
@@ -117,6 +118,7 @@ async def playwright_harness(
             if enable_clipboard
             else None,
             storage_state=storage_state,
+            servie_workers=disable_service_workers,
         )
 
         ctx.set_default_timeout(default_timeout)
